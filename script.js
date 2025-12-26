f558fe9c00a7b0e20e39503ab61b19097b82bdae
wolfriverside <128236161+wolfriverside@users.noreply.github.com>
2025-12-26 19:14:43 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const display = document.getElementById("display")
+
+function append(value) {
+  display.value += value
+}
+
+function clearDisplay() {
+  display.value = ""
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value)
+  } catch {
+    display.value = "Error"
+  }
+}
